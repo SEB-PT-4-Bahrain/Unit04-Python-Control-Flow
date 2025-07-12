9e62d558bdacb6e2703a36b1544ea34a16a5e373
omarakamal <omaraakamal@gmail.com>
2025-07-12 13:01:30 +0300

    update

--- class-code/control_flow.py
+++ class-code/control_flow.py
@@ -29,8 +29,8 @@ else:
     print("You are good to drive")
 
 
-my_country = input("Please give your country").lower()
-print(f"Wow what a great country {my_country}")
+# my_country = input("Please give your country").lower()
+# print(f"Wow what a great country {my_country}")
 
 # color = input('Enter "green", "yellow", "red": ').lower()
 # print(f'The user entered {color}')
@@ -54,5 +54,44 @@ print(f"Wow what a great country {my_country}")
 
 students = ["Sayed","Youseph","Isra","Malak"]
 
-for student in students:
-    print(student)
+for one_student in students:
+    print(one_student)
+
+
+num = 1
+while num <=10:
+    print(num)
+    num+= 1
+
+students = ["Sayed","Youseph","Isra","Malak"]
+
+for one_student in students:
+    if one_student == "Isra":
+        break
+    elif one_student == "Sayed":
+        continue
+    print(one_student + " Is here")
+
+
+
+
+# ranges:
+
+# start at 0 end at 5
+for num in range(5):
+    print(num)
+
+print("-----------")
+#  start at 1 and end at 5 (not including)
+for num in range(1,5):
+    print(num)
+print("-----------")
+
+
+# (start, stop, step)
+for num in range(1,10,4):
+    print(num)
+
+# going from end to start
+for num in range(10,1,-3):
+    print(num)
